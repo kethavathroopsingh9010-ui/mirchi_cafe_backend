3740943cd416b2114ae1e88e0d5f5cc2b740ee68
Kethavath Roopsingh <kethavathroopsingh9010@gmail.com>
2026-06-15 22:29:22 +0530

    feat(categories): add category management module

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Param,
+} from '@nestjs/common';
+
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(
+    private readonly categoriesService: CategoriesService,
+  ) {}
+
+  @Post()
+  create(@Body() dto: CreateCategoryDto) {
+    return this.categoriesService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.categoriesService.findOne(id);
+  }
+}
---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+} from 'typeorm';
+
+import { Product } from '../../products/entities/product.entity';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products!: Product[];
+}
